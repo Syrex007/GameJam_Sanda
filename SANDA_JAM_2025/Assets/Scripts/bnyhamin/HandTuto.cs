cc9b61136119e97cbfdd95ae2b471ed103676aaa
Benjamín Infantes <bnyhamin@gmail.com>
2025-08-22 14:25:03 -0500

    Guia del Tuto por bnyhamin

---
+++ SANDA_JAM_2025/Assets/Scripts/bnyhamin/HandTuto.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using UnityEngine;
+
+public class HandTuto : MonoBehaviour
+{
+    [Tooltip("Objeto que seleccionará inicialmente.")]
+    public GameObject goItem;
+
+    [Tooltip("Objeto que se desea instanciar al llegar al punto final.")]
+    public GameObject goPrefabItemInstanciar;
+
+    [Tooltip("Tiempo de espera al llegar al punto final antes de pasar al siguiente objeto.")]
+    public int waitTimePointEnd;
+
+    [Tooltip("Objeto siguiente que se desea activar al llegar al punto final.")]
+    public GameObject goNextHandActive = null;    
+
+    [Header("Configuración")]
+    [Tooltip("Desde la animación activará este check cuando pase por el pull.")]
+    public bool checkAnimItem = false;
+    [Tooltip("Desde la animación activará este check cuando llegue al punto final para que el player se acerque.")]
+    public bool checkMovePlayer = false;
+    [Tooltip("Desde la animación activará este check para indicar que ya terminó y podría pasar al siguiente HandTuto.")]
+    public bool checkInactive = false;
+    private int countSelectItem = 0;
+    private int countMoveItem = 0;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (checkAnimItem)
+        {
+            
+            selectItem();
+            countSelectItem++;
+
+        }
+
+        if (checkMovePlayer)
+        {
+            instanciarItem();
+            countMoveItem++;
+        }
+
+        
+        if (checkInactive)
+        {
+            StartCoroutine(waitInactive());
+        }
+
+    }
+
+
+    
+
+
+    public void selectItem() {
+        
+        if (countSelectItem == 0 && goItem != null)
+        {
+            var selectable = goItem.GetComponent<UI_SelectableItem>();
+            if (selectable != null)
+            {
+                selectable.SelectAnimation();
+                selectable.UseItem();
+            }
+        }
+        checkAnimItem = false;
+
+    }
+
+    public void instanciarItem()
+    {
+        if (countMoveItem == 0) Instantiate(goPrefabItemInstanciar, transform);
+        checkMovePlayer = false;
+
+    }
+
+
+
+    IEnumerator waitInactive()
+    {
+        checkInactive = false;
+        yield return new WaitForSeconds(waitTimePointEnd);
+        gameObject.SetActive(false);
+        if (goNextHandActive) goNextHandActive.SetActive(true);
+
+    }
+
+}
