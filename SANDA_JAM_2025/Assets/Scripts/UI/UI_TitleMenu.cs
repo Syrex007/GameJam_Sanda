970385f64dc0750335f4d8454264855f15a0815f
ElJulioGG <68710147+ElJulioGG@users.noreply.github.com>
2025-08-24 17:45:55 -0500

    fix

--- SANDA_JAM_2025/Assets/Scripts/UI/UI_TitleMenu.cs
+++ SANDA_JAM_2025/Assets/Scripts/UI/UI_TitleMenu.cs
@@ -5,14 +5,20 @@ public class UI_TitleMenu : MonoBehaviour
 {
     private UI_TweenEffects tweenEffects;
 
+    
+
     void Start()
     {
         tweenEffects = GetComponent<UI_TweenEffects>();
         tweenEffects.PlayFloatEffect();
+        SoundFXManager.instance.PlaySoundByName("TemaMenu", transform);
     }
 
     public void goToLevel()
     {
+        // Stop menu music before loading the scene
+        SoundFXManager.instance.StopSoundByName("TemaMenu");
+
         SceneManager.LoadScene("Nivel_0");
     }
 }
