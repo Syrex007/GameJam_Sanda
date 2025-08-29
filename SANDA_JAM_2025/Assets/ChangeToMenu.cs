c66f737ad8d2fa05249a307e94829f76eab3821d
Syrex007 <francodavid.latorre.vidal@gmail.com>
2025-08-29 00:42:05 -0500

    Arreglado BUG

--- SANDA_JAM_2025/Assets/ChangeToMenu.cs
+++ SANDA_JAM_2025/Assets/ChangeToMenu.cs
@@ -8,7 +8,8 @@ public class ChangeToMenu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void ReturnMenu()
     {
-        Time.timeScale = 1f;
+        Time.timeScale = 1f; // Start the game time
+        GameManager.instance.isGamePaused = false;
         SoundFXManager.instance.StopSoundByName("Item1");
         SoundFXManager.instance.StopSoundByName("Item2");
         SoundFXManager.instance.StopSoundByName("Item3");
