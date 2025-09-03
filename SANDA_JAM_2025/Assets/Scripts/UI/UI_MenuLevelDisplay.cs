822f24ee84a331eff5e0abc0156da8ae86f12132
Syrex007 <francodavid.latorre.vidal@gmail.com>
2025-09-03 05:27:21 -0500

    Re Assets

--- SANDA_JAM_2025/Assets/Scripts/UI/UI_MenuLevelDisplay.cs
+++ SANDA_JAM_2025/Assets/Scripts/UI/UI_MenuLevelDisplay.cs
@@ -15,9 +15,9 @@ public class UI_MenuLevelDisplay : MonoBehaviour
 
         int minutes = (int)(time / 60);
         int seconds = (int)(time % 60);
-        int milliseconds = (int)((time - Mathf.Floor(time)) * 1000);
+        int milliseconds = (int)(((time - Mathf.Floor(time)) * 10) % 10);
 
-        timeText.text = $"{minutes:00}:{seconds:00}:{milliseconds:00}";
+        timeText.text = $"{minutes:00}:{seconds:00}:{milliseconds}";
 
         // Update stars
         for (int i = 0; i < starFills.Length; i++)
